e7968fc4cf0d089500f310690d48802b01a5a4aa
jailsonsf <jailsonsoares002@gmail.com>
2019-06-11 16:20:52 -0300

    Update project :rocket:

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 30px;
+`;
+
+export const Top = styled.View`
+`;
+
+export const Logo = styled.View`
+`;
+
+export const Title = styled.View`
+`;
